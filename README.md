e7cf9aa737994c3aa38e6b5f5e9d410fd9dcd53e
webcodeshubham <theshubhamshri@gmail.com>
2024-12-30 22:54:46 +0530

    react - 1

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # cuvette_Oct_ReactJS_2025
+
+React JS => JavaScript Library => Create UIs.
+Single Page Application
+Virtual DOM
+Best in Performance
+JSX Syntax
+One-way Data Flow
---
+++ myfirstreactapp/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  const name = "Govind!";
+  return (
+    <>
+      <h1>Hello, I am Shubham!</h1>
+      <p>{23 + 78}</p>
+      <h1>{name}</h1>
+    </>
+  );
+}
+
+export default App;
